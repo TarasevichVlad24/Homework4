47dcef94375b16cbf64ea9af4cdcb9867977575d
TarasevichVlad24 <gagagdet@gmail.com>
2022-12-01 15:46:11 +0200

    Ex4

--- src/Group2/Ex4.java
+++ src/Group2/Ex4.java
@@ -6,29 +6,39 @@ import java.util.Scanner;
 public class Ex4 {
     public static void main(String[] args) {
         int n = 0;
-        int nums = 0;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Введите число n удовлетворяющее 5<n<=10");
-        n = s.nextInt();
-
-        while (n>5) {
-            if (s.hasNextInt()) {
+        int a;
+        int j = 0;
+        do {
+            System.out.print("Введите число n исходя из условия 5<n<=10    ");
+            Scanner s = new Scanner(System.in);
+            if(s.hasNextInt()){
                 n = s.nextInt();
-                if (n < 5) {
-                    System.out.println("Введенное число неверно  ");
-                    s.next();
+                if(n>5 && n<=10){
+                    int[] mas1 = new int[n];
+                    for(int i = 0; i<mas1.length;i++){
+                        mas1[i] = (int)(Math.random()*(n+1));
+                        System.out.print(mas1[i]+" ");
+                        if(mas1[i]%2==0){
+                            j++;
+                        }
+                    }
+                    System.out.println();
+                    System.out.println("Оставляем только четные числа  ");
+                    if(j!=0){
+                        int[] mas2 = new int[j];
+                        j=0;
+                        for(int i = 0; i<mas1.length;i++){
+                            if(mas1[i]%2==0){
+                                mas2[j] = mas1[i];
+                                System.out.print(mas2[j]+" ");
+                                j++;
+                            }
+                        }
+                    }else{
+                        System.out.println("Четных чисел нет");
+                    }
                 }
-            } else {
-                System.out.println("Ошибка  ");
             }
-            int[] mas1 = new int[n];
-            for (int i = 0; i < mas1.length; i++) {
-                mas1[i] = (int) (Math.random() * (n + 1));
-                System.out.print(mas1[i]+" ");
-                if (mas1[i] % 2 == 0&mas1[i]!=0)nums++;
-            }
-            System.out.println(" ");
-
-        }
+        }while(n<5 && n>=10);
     }
 }
