35eb2c726a53f13efa10be5a1cbcfdc9d95d98aa
TarasevichVlad24 <gagagdet@gmail.com>
2022-12-01 20:30:06 +0200

    Ex0

--- src/Group1/Ex0.java
+++ src/Group1/Ex0.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class Ex0 {
     public static void main(String[] args) {
         int a;
+        int count = 0;
         Scanner s = new Scanner(System.in);
         System.out.println("Введите число");
         a = s.nextInt();
 
-        int[] num = new int[10];
+        int[] num = new int[]{1, 3, 4, 5, 8, 11, 14};
         for (int i = 0; i < num.length; i++) {
+
+            if (a == num[i]) {
+                count++;
+            } else {
+
+            }
         }
-        //if (a < num.legth) {
+        if (count > 0) {
             System.out.println("Число входит");
-        //} else {
+        } else {
             System.out.println("Число не входит");
         }
     }
-//}
+}
